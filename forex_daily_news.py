2296e332b9ed29f39563c84563959592f4c83a9c
diilanofficial2002 <adilanofficial2002@gmail.com>
2025-07-03 13:20:13 +0700

    modify timezone

--- forex_daily_news.py
+++ forex_daily_news.py
@@ -21,6 +21,10 @@ options.add_argument('--no-sandbox')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 
 driver = webdriver.Chrome(options=options)
+driver.execute_cdp_cmd(
+    'Emulation.setTimezoneOverride',
+    {'timezoneId': 'Asia/Bangkok'}  # ตั้ง timezone ที่ต้องการ
+)
 
 try:
     url = "https://www.forexfactory.com/"
